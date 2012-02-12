38c2a46ce6689c42a4586105bb57aa58db9bc25a
Eyal Shapira <eyal@wizery.com>
2012-02-13 01:59:31 +0200

    Add API and CLI command to control sched scan intervals (UPSTREAM)
    Enable control over the sched scan intervals parameters
    through a new wpa_s API (SCHED_SCAN_INTERVALS) and a
    matching wpa_cli command.
    
    Signed-off-by: Eyal Shapira <eyal@wizery.com>

--- wpa_supplicant/config.c
+++ wpa_supplicant/config.c
@@ -3141,9 +3141,10 @@ static const struct global_parse_data global_fields[] = {
 	{ INT(beacon_int), 0 },
 	{ FUNC(ap_vendor_elements), 0 },
 	{ INT_RANGE(ignore_old_scan_res, 0, 1), 0 },
-	{ INT_RANGE(sched_scan_num_short_intervals, 0, 14), 0 },
-	{ INT_RANGE(sched_scan_short_interval, 1, 3600), 0 },
-	{ INT_RANGE(sched_scan_long_interval, 1, 3600), 0 },
+	{ INT_RANGE(sched_scan_num_short_intervals, 0,
+		    MAX_NUM_SCHED_SCAN_SHORT_INTERVALS), 0 },
+	{ INT_RANGE(sched_scan_short_interval, 1, MAX_SCHED_SCAN_INTERVAL), 0 },
+	{ INT_RANGE(sched_scan_long_interval, 1, MAX_SCHED_SCAN_INTERVAL), 0 },
 };
 
 #undef FUNC
