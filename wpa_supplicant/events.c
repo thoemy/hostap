03b8b42b152e7cac651e36aa1554a883de0e7d4a
Eyal Shapira <eyal@wizery.com>
2012-02-17 00:25:14 +0200

    Ignore a deauth from a different BSS than the current one
    This fixes an issue with roaming where the previous
    BSS would send a DEAUTH during the roaming and this
    would caused the supplicant to disconnect.

--- wpa_supplicant/events.c
+++ wpa_supplicant/events.c
@@ -1955,7 +1955,8 @@ static int could_be_psk_mismatch(struct wpa_supplicant *wpa_s, u16 reason_code,
 
 static void wpa_supplicant_event_disassoc_finish(struct wpa_supplicant *wpa_s,
 						 u16 reason_code,
-						 int locally_generated)
+						 int locally_generated,
+						 const u8 *addr)
 {
 	const u8 *bssid;
 	int authenticating;
@@ -1975,6 +1976,13 @@ static void wpa_supplicant_event_disassoc_finish(struct wpa_supplicant *wpa_s,
 		 */
 		wpa_dbg(wpa_s, MSG_DEBUG, "Disconnect event - ignore in "
 			"IBSS/WPA-None mode");
+		return;
+	}
+
+	if (addr && wpa_s->bssid && os_memcmp(wpa_s->bssid, addr, ETH_ALEN)) {
+		/* This may occur during roaming */
+		wpa_dbg(wpa_s, MSG_DEBUG, "Ignore disconnect from"
+			" a BSS which is not the current one");
 		return;
 	}
 
@@ -2542,7 +2550,8 @@ void wpa_supplicant_event(void *ctx, enum wpa_event_type event,
 				wpas_auth_failed(wpa_s);
 
 			wpa_supplicant_event_disassoc_finish(wpa_s, reason_code,
-							     locally_generated);
+							     locally_generated,
+							     data->deauth_info.addr);
 		}
 		break;
 	case EVENT_MICHAEL_MIC_FAILURE:
